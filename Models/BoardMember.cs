67c2894098521bda947569c0a4776cddf5fbf26a
Mateusz Gozdek <gozdeusz@gmail.com>
2026-06-17 18:47:08 +0200

    feat: add database models

---
+++ Models/BoardMember.cs
@@ -0,0 +1,19 @@
+﻿namespace KanbanApi.Models
+{
+    public class BoardMember
+    {
+        public ApplicationUser User { get; set; }
+        public Board Board { get; set; }
+        public Role Role { get; set; }
+
+        public string UserId { get; set; }
+        public int BoardId { get; set; }
+
+    }
+
+    public enum Role
+    {
+        Owner,
+        Member
+    }
+}
---
+++ Models/Column.cs
@@ -0,0 +1,10 @@
+﻿namespace KanbanApi.Models
+{
+    public class Column
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Card> Cards { get; set; }
+        public Board BoardId { get; set; }
+    }
+}
